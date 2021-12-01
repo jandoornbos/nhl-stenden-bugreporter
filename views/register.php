76cea2d3ac592e28c657d7a89ff6adeedb2d9a3d
Jan Doornbos <jan.doornbos@nhlstenden.com>
2021-12-01 10:45:32 +0100

    Improve register page design

--- views/register.php
+++ views/register.php
@@ -1,6 +1,13 @@
-<form method="post">
-    <input type="text" class="form-control" name="username">
-    <input type="password" class="form-control" name="password">
-    <input type="hidden" name="action" value="register">
-    <button type="submit" class="btn btn-success">Register</button>
-</form>
+<div class="jumbotron">
+    <h1 class="display-4">Bugreporter</h1>
+    <h4>Register</h4>
+    <hr>
+    <form method="post">
+        <label for="username">Username</label>
+        <input type="text" class="form-control" name="username" id="username">
+        <label for="password">Password</label>
+        <input type="password" class="form-control" name="password" id="password">
+        <input type="hidden" name="action" value="register">
+        <input type="submit" class="btn btn-success mt-3" value="Register">
+    </form>
+</div>
